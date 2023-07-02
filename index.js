61707de61d80b311cc623ff9893de0305085032c
Bryan B. Dorneles <bryan_dorneles@hotmail.com>
2023-07-02 23:42:31 +1000

    port updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const port = 3010; // Choose the desired port number
+const port = 3000; // Choose the desired port number
 
 // MySQL database connection configuration
 const connection = mysql.createConnection({
