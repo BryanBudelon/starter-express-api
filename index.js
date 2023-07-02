d19553b3c9b013f31cb03533f0b52b0659d1a755
Bryan B. Dorneles <bryan_dorneles@hotmail.com>
2023-07-03 00:00:23 +1000

    updating pool connection

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000; // Choose the desired port number
 
 // MySQL database connection configuration
 const connection = mysql.createPool({
+    connectionLimit: 10,
   host: '149.28.189.125',
   user: 'awkdrhpgbw',
   password: '9YKC2thkPk',
@@ -15,19 +16,19 @@ const connection = mysql.createPool({
 let baseURL = 'https://studdog.app/wp-content'; 
 
 // Connect to the MySQL database
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to the database: ', err);
-      return;
-    }
-    console.log('Connected to the database');
-  });
+//   connection.connect((err) => {
+//     if (err) {
+//       console.error('Error connecting to the database: ', err);
+//       return;
+//     }
+//     console.log('Connected to the database');
+//   });
 
   // Define a route to fetch data
-  app.get('/api/posts', (req, res) => {
+app.get('/api/posts', (req, res) => {
     const firstQuery = "SELECT post_name FROM wp_posts WHERE post_type = 'rz_listing' AND post_status = 'publish'";
     
-    connection.query(firstQuery, (error, results) => {
+    pool.query(firstQuery, (error, results) => {
       if (error) {
         console.error('Error executing the first query: ', error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -38,7 +39,7 @@ let baseURL = 'https://studdog.app/wp-content';
       
       const secondQuery = `SELECT * FROM wp_postmeta WHERE post_id IN (SELECT ID FROM wp_posts WHERE post_type = 'rz_listing' AND post_status = 'publish' AND post_name IN (${postNames.map(() => '?').join(',')}))`;
       
-      connection.query(secondQuery, postNames, (error, results) => {
+      pool.query(secondQuery, postNames, (error, results) => {
         if (error) {
           console.error('Error executing the second query: ', error);
           res.status(500).json({ error: 'Internal Server Error' });
@@ -49,18 +50,17 @@ let baseURL = 'https://studdog.app/wp-content';
       });
     });
   });
-
-
+  
   app.get('/api/posts/:postID', (req, res) => {
     let postID = req.params.postID;
-
-    let sql = `SELECT meta_value FROM wp_postmeta WHERE post_id = ${postID} AND meta_key = 'rz_gallery'`;
-
-    connection.query(sql, function(err, result) {
+  
+    let sql = `SELECT meta_value FROM wp_postmeta WHERE post_id = ? AND meta_key = 'rz_gallery'`;
+  
+    pool.query(sql, [postID], function(err, result) {
         if (err) throw err;
-
+  
         let metaValue = result[0].meta_value;
-
+  
         let galleryImages;
         try {
             galleryImages = JSON.parse(metaValue);
@@ -68,32 +68,30 @@ let baseURL = 'https://studdog.app/wp-content';
             console.error('Error parsing meta_value JSON: ', error);
             return res.status(500).json({ error: 'Error parsing meta_value JSON' });
         }
-
+  
         let imageIDs = galleryImages.map(item => item.id);
-
+  
         let imageLinks = [];
         for(let i = 0; i < imageIDs.length; i++) {
             let imageID = imageIDs[i];
-            let sql = `SELECT guid FROM wp_posts WHERE ID = ${imageID} AND post_type = 'attachment'`;
-            connection.query(sql, function(err, result) {
+            let sql = `SELECT guid FROM wp_posts WHERE ID = ? AND post_type = 'attachment'`;
+            pool.query(sql, [imageID], function(err, result) {
                 if (err) throw err;
-
+  
                 if (result.length > 0) {
                     let filePath = result[0].guid;
                     let imageURL = baseURL + filePath.split('wp-content')[1];
                     imageLinks.push(imageURL);
                 }
-
+  
                 if (i == imageIDs.length - 1) {
                     res.send(imageLinks);
                 }
             });
         }
     });
-});
-
+  });
   
-
   // Start the server
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
