0501b29f0cf8d62e09ea1b268c06914215e60f9a
Bryan B. Dorneles <bryan_dorneles@hotmail.com>
2023-07-03 11:49:55 +1000

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
 const port = 3000; // Choose the desired port number
+
+app.use(cors());
 
 // MySQL database connection configuration
 
